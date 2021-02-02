d758d9f6ca84886d7d53869bdb50576b9c27fc39
Guillermo Moreno Rivera <morenorivera1234@gmail.com>
2021-02-02 17:38:56 -0600

    Función seno
    Incluí la función para convertir de grados a radianes, calcular un factorial, calcular el error meta y el actual y la función seno en grados (Creo)

--- Calculadora.java
+++ Calculadora.java
@@ -5,21 +5,28 @@ Guillermo Moreno Rivera y Luis Fernando Chávez Jiménez
 import java.util.Scanner;
 public class Calculadora{
     double PI = 3.14159265;
+    static int errorMeta = 0;
+    static double aproxAct = 0, aproxAnt = 0;
     public static void main (String []args){
         Scanner sc = new Scanner(System.in);
         int opc = 1;
+        
+        System.out.print("Ingrese el número de cifras significativas precisas que desea en las operaciones: ");
+        errorMeta = sc.nextInt();
         while(opc != 0){
             System.out.println("1.- Calcular seno");
             System.out.println("2.- Calcular coseno");
             System.out.println("3.- Calcular logaritmo");
             System.out.println("4.- Calcular raíz cuadrada");
             System.out.println("5.- Calcular exponencial");
+            System.out.println("6.- Cambiar presición");
             System.out.println("0.- Salir");
             System.out.println("Ingrese una opción: ");
             opc = sc.nextInt();
             switch(opc){
                 case 1:
-
+                    System.out.println("Cálculo del seno");
+                    aproxAnt = 1;
                 break;
                 case 2:
                 break;
@@ -29,6 +36,10 @@ public class Calculadora{
                 break;
                 case 5:
                 break;
+                case 6:
+                    System.out.print("Ingrese el número de cifras significativas precisas: ");
+                    errorMeta = sc.nextInt();
+                break;
                 case 0:
                 break;
                 default:
@@ -37,7 +48,31 @@ public class Calculadora{
         }
         sc.close();
     }
+    //Convierte una cantidad de grados a radianes.
     public double toRadian(double cant){
         return (cant * (PI/180));
+    }
+    //Calcula el error meta
+    public double decimalErrorMeta(int errorMeta){
+        return .5*(10^(2-errorMeta));
+    }
+    //Calcula el error actual.
+    public double errorActual(double aproxAct, double aproxAnt){
+        return (aproxAct-aproxAnt)/aproxAct;
+    }
+    public double seno(int x, int aproxAnte){
+        int n = 0;
+        do{
+            aproxAct = (((-1)^n)*(x^(2*(n + 1))))/factorial((2*n)+1);
+        }while(errorActual(aproxAct, aproxAnt)<decimalErrorMeta(errorMeta));
+        return aproxAct;
+    }
+    public double factorial(int x){
+        int fact = 1;
+        while(x>0){
+            fact = fact * x;
+            x--;
+        }
+        return fact;
     }
 }
